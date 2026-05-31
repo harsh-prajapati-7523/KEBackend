b6d0adcf7ccb1eeb3497a16e4ca9caf5018216ac
Harsh Prajapati <harsh.prajapati7523@gmail.com>
2026-05-31 14:47:03 +0530

    dev: improved schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,24 +2,24 @@
 INSERT INTO employees (name, employee_Id, password, role)
 VALUES (
     'Harsh Prajapati(SUPER_ADMIN)',
-    'SUPER_ADMIN001',
+    'SUPER_ADMIN_001',
     '$2a$10$mKW.pIeXm60F8D7u9G38Qu8.4ueYo.N87GNcfEWhNzvpn44Xis726',
     'SUPER_ADMIN'
-);
+) ON CONFLICT (employee_id) DO NOTHING;;
 INSERT INTO employees (name, employee_Id, password, role)
 VALUES (
     'Harsh Prajapati(Admin)',
     'ADMIN_001',
     '$2a$10$mKW.pIeXm60F8D7u9G38Qu8.4ueYo.N87GNcfEWhNzvpn44Xis726',
     'ADMIN'
-);
+) ON CONFLICT (employee_id) DO NOTHING;;
 INSERT INTO employees (name, employee_Id, password, role)
 VALUES (
     'Harsh Prajapati(Technician)',
     'TECHNICIAN_001',
     '$2a$10$mKW.pIeXm60F8D7u9G38Qu8.4ueYo.N87GNcfEWhNzvpn44Xis726',
     'TECHNICIAN'
-);
+) ON CONFLICT (employee_id) DO NOTHING;;
 
 --Ticket Number Sequence Creation
 CREATE SEQUENCE IF NOT EXISTS ticket_number_seq
